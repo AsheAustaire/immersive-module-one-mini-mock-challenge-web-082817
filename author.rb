a2dfa420815e9b6e11eba39ec61b7a05f37f5ac7
Ashe Austaire <AsheAustaire@gmail.com>
2017-09-08 11:24:38 -0400

    my version

--- author.rb
+++ author.rb
@@ -1,1 +1,32 @@
+class Author
 
+    @@all = []
+
+    attr_accessor :books
+    attr_reader :total_words
+
+    def initialize(books)
+        @books = []
+        @total_words = 0
+        @@all << self
+    end
+
+    def self.all()
+        @@all
+    end
+
+    def write_book(title, word_count)
+        new_book = Book.new(title, self, word_count)
+        @total_words += word_count
+    end
+
+    def self.most_words()
+        highest = 0
+        winner = nil
+        Author.all.each do |author_o|
+            highest = author_o.total_words if author_o.total_words > highest
+            winner = author_o if author_o.total_words > highest
+        end
+        winner
+    end
+end
--- book.rb
+++ book.rb
@@ -1,1 +1,21 @@
+class Book
 
+    attr_accessor :author
+    attr_reader :title, :word_count
+
+
+    @@books = []
+
+    def initialize(title, author, word_count)
+        @title = title
+        @author = author
+        @word_count = word_count
+        author.books << self #initializes a book into an author object.
+        @@books << self
+    end
+
+    def self.all() #class getter for @@books
+        @@books
+    end
+
+end
